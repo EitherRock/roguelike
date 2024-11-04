d1f89dd0f020fa28450767a15010bc423f01275a
EitherRock <38919317+EitherRock@users.noreply.github.com>
2024-11-03 18:40:46 -0600

    ai and redo keys

--- entity.py
+++ entity.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 import copy
-from typing import Optional, Tuple, TypeVar, TYPE_CHECKING
+from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING
+
+from render_order import RenderOrder
 
 if TYPE_CHECKING:
+    from components.ai import BaseAI
+    from components.fighter import Fighter
     from game_map import GameMap
 
 T = TypeVar("T", bound="Entity")
@@ -22,7 +26,8 @@ class Entity:
             char: str = "?",
             color: Tuple[int, int, int] = (255, 255, 255),
             blocks_movement: bool = False,
-            name: str = "<Unnamed>"
+            name: str = "<Unnamed>",
+            render_order: RenderOrder = RenderOrder.CORPSE
 
     ):
         self.x = x
@@ -55,8 +60,40 @@ class Entity:
             self.gamemap = gamemap
             gamemap.entities.add(self)
 
-
     def move(self, dx: int, dy: int) -> None:
         # Move the entity by a given amount
         self.x += dx
         self.y += dy
+
+
+class Actor(Entity):
+
+    def __init__(
+        self,
+        *,
+        x: int = 0,
+        y: int = 0,
+        char: str = "?",
+        color: Tuple[int, int, int] = (255, 255, 255),
+        name: str = "<Unnamed>",
+        ai_cls: Type[BaseAI],
+        fighter: Fighter
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            char=char,
+            color=color,
+            name=name,
+            blocks_movement=True,
+        )
+
+        self.ai: Optional[BaseAI] = ai_cls(self)
+
+        self.fighter = fighter
+        self.fighter.entity = self
+
+    @property
+    def is_alive(self) -> bool:
+        """Returns True as long as this actor can perform actions."""
+        return bool(self.ai)
